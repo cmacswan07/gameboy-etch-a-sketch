ae4cead8fb9e81833c3cef3b75fd7e996c294dd5
cmacswan07 <cmacswan07@gmail.com>
2018-06-11 19:29:03 -0700

    Grid coloring now works.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,34 @@
 const container = document.querySelector('#container');
-
 const row = document.createElement('div');
 const cell = document.createElement('div');
-cell.setAttribute('style', 'display : inline-block; border : 1px solid black; height : 25px; width : 25px; ');
-row.setAttribute('style', 'width : 400px;')
-
-container.appendChild(row)
 
 
+/* Creates grid. */
 for (a=0; a<16; a++) {
 	const row = document.createElement('div');
-	row.setAttribute('style', 'width : 400px;');
+	row.setAttribute('style', 'height : 25px; width : 400px; box-sizing : border-box;');
 	container.appendChild(row);
-	console.log('made row');
 
 		for (i=0; i<16; i++) {
 			const cell = document.createElement('div');
 			cell.setAttribute("style", 
-			'display : inline-block; border : .25px solid black; height : 25px; width : 25px; box-sizing : border-box; ');	
+			'display : inline-block; border : .25px solid black; height : 25px; width : 25px; box-sizing : border-box; ');
+			cell.addEventListener("mouseover", () => { cell.style.backgroundColor = "pink"; });
+			cell.classList.add('cell');	
 			row.appendChild(cell); }
 }
 
-
+function darkenCell() {
+	let currentDarkness = this.style.opacity;
+	let newDarkness = Number(currentDarkness) + 0.1;
+	this.style.currentDarkness = newDarkness;
+}
 
 /*
 
+			cell.style.backgroundColor = "red";
 */
+
 
 
 
