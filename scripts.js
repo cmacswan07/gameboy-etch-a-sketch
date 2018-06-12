4164c2773604dfd209ca8ced07cd6cefe8826c2c
cmacswan07 <cmacswan07@gmail.com>
2018-06-11 18:23:58 -0700

    Created grid.

--- scripts.js
+++ scripts.js
@@ -8,13 +8,24 @@ row.setAttribute('style', 'width : 400px;')
 container.appendChild(row)
 
 
+for (a=0; a<16; a++) {
+	const row = document.createElement('div');
+	row.setAttribute('style', 'width : 400px;');
+	container.appendChild(row);
+	console.log('made row');
 
-for (i=0; i<16; i++) {
-	const cell = document.createElement('div');
-	cell.setAttribute("style", 
-		'display : inline-block; border : 1px solid black; height : 25px; width : 25px; box-sizing : border-box; ');	
-	row.appendChild(cell); }
+		for (i=0; i<16; i++) {
+			const cell = document.createElement('div');
+			cell.setAttribute("style", 
+			'display : inline-block; border : .25px solid black; height : 25px; width : 25px; box-sizing : border-box; ');	
+			row.appendChild(cell); }
+}
 
+
+
+/*
+
+*/
 
 
 
