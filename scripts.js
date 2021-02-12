522aa39ef2aa59fa7afabc50e26719aa89de4d49
cmacswan07 <cmacswan07@gmail.com>
2021-02-12 12:27:23 -0800

    cleaned up code

--- scripts.js
+++ scripts.js
@@ -1,52 +1,46 @@
 const container = document.querySelector('#container');
 const row = document.createElement('div');
 const cell = document.createElement('div');
+const resizeButton = document.querySelector('#resize');
+const documentBody = document.getElementById('document-body');
+
 let gridSize = 20;
 
-let documentBody = document.getElementById('document-body');
-documentBody.addEventListener('touchstart', function() {}, true );
-
-createGrid();
-
-let resizeButton = document.querySelector('#resize');
-
-resizeButton.onclick = () => { 
-	container.innerHTML = "";
-	gridSize = prompt("Select a new size for grid. (Size X Size)");
-	createGrid();
-
-}
-
-
-
+/* creates grid */
 function createGrid() {
-	for (a=0; a<gridSize; a++) {
+	for (var i = 0; i < gridSize; i++) {
 		const row = document.createElement('div');
-		row.setAttribute('style', 'height :' + 300/gridSize + 'px; width : 300px;');
+		row.setAttribute('style', 'height :' + 300 / gridSize + 'px; width : 300px;');
 		row.classList.add('row');
 		container.appendChild(row);
 
-			for (i=0; i<gridSize; i++) {
-				const cell = document.createElement('div');
-				cell.setAttribute("style", 
-				'display : table-cell; height :' + 300/gridSize + 'px; width :' + 300/gridSize + 'px; opacity : 0; background-color : #000');
-				cell.addEventListener("mouseover", darkenCell);
-				cell.classList.add('cell');	
-				row.appendChild(cell); }
+		for (var j = 0; j < gridSize; j++) {
+			const cell = document.createElement('div');
+			cell.setAttribute("style", 
+			'display : table-cell; height :' + 300/gridSize + 'px; width :' + 300/gridSize + 'px; opacity : 0; background-color : #000');
+			cell.addEventListener("mouseover", darkenCell);
+			cell.classList.add('cell');	
+			row.appendChild(cell); 
+		}
 	}
 }
 
 /* Increases opacity of cells. */
 function darkenCell () {
-
-	let cellOpacity = +this.style.opacity;
+	let cellOpacity = this.style.opacity++;
 
 	if (cellOpacity < 1) {
 		cellOpacity += 0.1;
 	}
 
 	this.style.opacity = cellOpacity;
-
 }
 
+resizeButton.onclick = () => { 
+	container.innerHTML = "";
+	gridSize = prompt("Select a new size for grid. (Size X Size)");
+	createGrid();
+}
+
+createGrid();
 
