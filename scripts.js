8f27a9efb6e062328417ad081c24e5b476e7bade
cmacswan07 <cmacswan07@gmail.com>
2021-02-24 10:54:46 -0800

    cleanup

--- scripts.js
+++ scripts.js
@@ -8,39 +8,38 @@ let gridSize = 20;
 
 /* creates grid */
 function createGrid() {
-	for (var i = 0; i < gridSize; i++) {
-		const row = document.createElement('div');
-		row.setAttribute('style', 'height :' + 300 / gridSize + 'px; width : 300px;');
-		row.classList.add('row');
-		container.appendChild(row);
+  for (var i = 0; i < gridSize; i++) {
+    const row = document.createElement('div');
+    row.setAttribute('style', 'height :' + 300 / gridSize + 'px; width : 300px;');
+    row.classList.add('row');
+    container.appendChild(row);
 
-		for (var j = 0; j < gridSize; j++) {
-			const cell = document.createElement('div');
-			cell.setAttribute("style", 
-			'display : table-cell; height :' + 300/gridSize + 'px; width :' + 300/gridSize + 'px; opacity : 0; background-color : #000');
-			cell.addEventListener("mouseover", darkenCell);
-			cell.classList.add('cell');	
-			row.appendChild(cell); 
-		}
-	}
+    for (var j = 0; j < gridSize; j++) {
+      const cell = document.createElement('div');
+      cell.setAttribute("style", 
+      'display : table-cell; height :' + 300/gridSize + 'px; width :' + 300/gridSize + 'px; opacity : 0; background-color : #000');
+      cell.addEventListener("mouseover", darkenCell);
+      cell.classList.add('cell');	
+      row.appendChild(cell); 
+    }
+  }
 }
 
 /* Increases opacity of cells. */
 function darkenCell () {
-	let cellOpacity = this.style.opacity++;
+  let cellOpacity = this.style.opacity++;
 
-	if (cellOpacity < 1) {
-		cellOpacity += 0.1;
-	}
+  if (cellOpacity < 1) {
+    cellOpacity += 0.1;
+  }
 
-	this.style.opacity = cellOpacity;
+  this.style.opacity = cellOpacity;
 }
 
 resizeButton.onclick = () => { 
-	container.innerHTML = "";
-	gridSize = prompt("Select a new size for grid. (Size X Size)");
-	createGrid();
+  container.innerHTML = "";
+  gridSize = prompt("Select a new size for grid. (Size X Size)");
+  createGrid();
 }
 
 createGrid();
-
